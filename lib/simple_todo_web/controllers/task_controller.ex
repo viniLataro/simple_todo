9f16cf270c3682133f70948cd7d53e2040345cf7
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-04-11 23:44:34 -0300

    add todo task json

---
+++ lib/simple_todo_web/controllers/task_controller.ex
@@ -0,0 +1,43 @@
+defmodule SimpleTodoWeb.TaskController do
+  use SimpleTodoWeb, :controller
+
+  alias SimpleTodo.Todo
+  alias SimpleTodo.Todo.Task
+
+  action_fallback SimpleTodoWeb.FallbackController
+
+  def index(conn, _params) do
+    tasks = Todo.list_tasks()
+    render(conn, :index, tasks: tasks)
+  end
+
+  def create(conn, %{"task" => task_params}) do
+    with {:ok, %Task{} = task} <- Todo.create_task(task_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", ~p"/api/tasks/#{task}")
+      |> render(:show, task: task)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    task = Todo.get_task!(id)
+    render(conn, :show, task: task)
+  end
+
+  def update(conn, %{"id" => id, "task" => task_params}) do
+    task = Todo.get_task!(id)
+
+    with {:ok, %Task{} = task} <- Todo.update_task(task, task_params) do
+      render(conn, :show, task: task)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    task = Todo.get_task!(id)
+
+    with {:ok, %Task{}} <- Todo.delete_task(task) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
